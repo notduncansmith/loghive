f7e88776602c9eb2ee86f8e03285a9345c8483b8
Duncan Smith <duncan@dubsado.com>
2019-12-01 09:58:27 -0800

    Remove dead code

--- timestamps.go
+++ timestamps.go
@@ -8,12 +8,6 @@ func timestampNow() time.Time {
 	return time.Now().UTC()
 }
 
-func timeFromBytes(bz []byte) time.Time {
-	t := time.Time{}
-	t.UnmarshalText(bz)
-	return t
-}
-
 func timeToBytes(t time.Time) []byte {
 	bz, _ := t.MarshalText()
 	return bz
