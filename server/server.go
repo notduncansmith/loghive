3fb52c7c4a2cccb5fbb9cb1480461211969229d0
Duncan Smith <duncan@dubsado.com>
2019-12-01 08:16:18 -0800

    Remove dead code

--- server/server.go
+++
@@ -1,8 +0,0 @@
-package server
-
-import "fmt"
-
-// StartServer starts the HTTPS server on a port with a given keyfile path
-func StartServer(port int, pathToKeyfile string) {
-	fmt.Printf("Starting server on port %v\n", port)
-}
