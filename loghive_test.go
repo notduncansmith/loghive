22df04b1d85b9d997be156cc66faefdbc5dc0385
Duncan Smith <hi@dms.sh>
2019-12-11 06:49:14 -0800

    Refactor tests to use withHive (like withSM), test iteration order

--- loghive_test.go
+++ loghive_test.go
@@ -21,15 +21,17 @@ type logstub struct {
 	line   string
 }
 
-func hive(t *testing.T, path string, domains []string) *Hive {
+func withHive(t *testing.T, path string, domains []string, f func(*Hive)) *Hive {
 	os.RemoveAll(path)
 	defer os.RemoveAll(path)
 	config := DefaultConfig
+	config.InternalLogLevel = logrus.DebugLevel
 	config.WritableDomains = domains
 	h, err := NewHive(path, config)
 	if err != nil {
 		t.Errorf("Should be able to create hive: %v", err)
 	}
+	f(h)
 	return h
 }
 
@@ -40,11 +42,11 @@ func stubLogs(t *testing.T, h *Hive, stubs []logstub) {
 		wg.Add(1)
 		flushed, err := h.Enqueue(stub.domain, []byte(stub.line))
 		expectSuccess(t, "enqueue message", err)
-		go func() {
-			err := <-flushed
+		go func(f chan error) {
+			err := <-f
 			expectSuccess(t, "write message", err)
 			wg.Done()
-		}()
+		}(flushed)
 	}
 
 	wg.Wait()
@@ -80,59 +82,89 @@ func checkResults(t *testing.T, h *Hive, q *Query, expected []logstub) {
 }
 
 func TestCreate(t *testing.T) {
-	hive(t, "./fixtures/create", []string{"test"})
-	os.Mkdir("./fixtures/unwritable", 0400)
-	defer os.Remove("./fixtures/unwritable")
-	h, err := NewHive("./fixtures/unwritable", DefaultConfig)
-	if h != nil || err == nil {
-		t.Error("Should not be able to create hive at unwritable dir")
-	}
+	withHive(t, "./fixtures/create", []string{"test"}, func(h *Hive) {
+		os.Mkdir("./fixtures/unwritable", 0400)
+		defer os.Remove("./fixtures/unwritable")
+		h, err := NewHive("./fixtures/unwritable", DefaultConfig)
+		if h != nil || err == nil {
+			t.Error("Should not be able to create hive at unwritable dir")
+		}
+	})
 }
 
 func TestRoundtripIterate(t *testing.T) {
-	h := hive(t, "./fixtures/roundtrip_iterate", []string{"test"})
-	stubLogs(t, h, []logstub{
-		logstub{"test", "foo"},
+	withHive(t, "./fixtures/roundtrip_iterate", []string{"test"}, func(h *Hive) {
+		stubLogs(t, h, []logstub{
+			logstub{"test", "foo"},
+		})
+
+		chans := h.sm.Iterate([]string{"test"}, timestamp().Add(time.Duration(-5)*time.Second), timestamp(), 1, 1)
+		read := <-(chans[0])
+		if len(read) != 1 {
+			t.Errorf("Expected to read 1 written msg, found %v", read)
+			t.FailNow()
+		}
+
+		if string(read[0].Line) != "foo" {
+			t.Errorf("Expected to find stored msg, found %v", read[0])
+		}
 	})
-
-	chans := h.sm.Iterate([]string{"test"}, timestamp().Add(time.Duration(-5)*time.Second), timestamp(), 1, 1)
-	read := <-(chans[0])
-	if len(read) != 1 {
-		t.Errorf("Expected to read 1 written msg, found %v", read)
-		t.FailNow()
-	}
-
-	if string(read[0].Line) != "foo" {
-		t.Errorf("Expected to find stored msg, found %v", read[0])
-	}
 }
 
 func TestRoundtripQuery(t *testing.T) {
-	h := hive(t, "./fixtures/roundtrip_query", []string{"test"})
-	stubLogs(t, h, []logstub{
-		logstub{"test", "foo"},
-	})
+	withHive(t, "./fixtures/roundtrip_query", []string{"test"}, func(h *Hive) {
+		stubLogs(t, h, []logstub{
+			logstub{"test", "foo"},
+			logstub{"test", "bar"},
+			logstub{"test", "baz"},
+		})
 
-	oneMinuteAgo := timestamp().Add(time.Duration(-1) * time.Minute)
-	q := NewQuery([]string{"test"}, oneMinuteAgo, timestamp(), FilterMatchAll())
+		oneMinuteAgo := timestamp().Add(time.Duration(-1) * time.Minute)
+		q := NewQuery([]string{"test"}, oneMinuteAgo, timestamp(), FilterMatchAll())
 
-	checkResults(t, h, q, []logstub{
-		logstub{"test", "foo"},
+		checkResults(t, h, q, []logstub{
+			logstub{"test", "foo"},
+			logstub{"test", "bar"},
+			logstub{"test", "baz"},
+		})
 	})
 }
 
 func TestEnqueueValidation(t *testing.T) {
-	h := hive(t, "./fixtures/roundtrip_query", []string{"test"})
+	withHive(t, "./fixtures/enqueue_validation", []string{"test"}, func(h *Hive) {
+		if _, err := h.Enqueue("nonexistentDomain", []byte("foo")); err == nil {
+			t.Error("Should not be able to log to non-existent domain")
+		}
 
-	if _, err := h.Enqueue("nonexistentDomain", []byte("foo")); err == nil {
-		t.Error("Should not be able to log to non-existent domain")
-	}
+		if _, err := h.Enqueue("test", nil); err == nil {
+			t.Error("Should not be able to log non-existent line")
+		}
 
-	if _, err := h.Enqueue("test", nil); err == nil {
-		t.Error("Should not be able to log non-existent line")
-	}
+		if _, err := h.Enqueue("test", make([]byte, 9*1024)); err == nil {
+			t.Errorf("Should not be able to log line of %v bytes", 9*1024)
+		}
+	})
+}
 
-	if _, err := h.Enqueue("test", make([]byte, 9*1024)); err == nil {
-		t.Errorf("Should not be able to log line of %v bytes", 9*1024)
+func TestErrWhileFlushing(t *testing.T) {
+	logrus.SetLevel(logrus.DebugLevel)
+
+	withHive(t, "./fixtures/err_while_flushing", []string{"test"}, func(h *Hive) {
+		flushed, err := h.Enqueue("test", []byte("shouldFail"))
+		expectSuccess(t, "enqueue", err)
+
+		for e := range flushed {
+			if e != nil {
+				err = e
+			}
+			logrus.Println(e)
+		}
+
+		if err == nil {
+			t.Error("Flush should have failed")
+		}
+	})
+}
+
 	}
 }
--- query_test.go
+++ query_test.go
@@ -6,54 +6,56 @@ import (
 )
 
 func TestQueryValidation(t *testing.T) {
-	h := hive(t, "./fixtures/roundtrip_query", []string{"test"})
-	now := timestamp()
-	oneMinuteFromNow := now.Add(time.Duration(1) * time.Minute)
-	twoMinutesFromNow := now.Add(time.Duration(2) * time.Minute)
-	err := h.Query(NewQuery([]string{"test"}, oneMinuteFromNow, twoMinutesFromNow, FilterMatchAll()))
-	if err == nil {
-		t.Error("Should not be able to query the future")
-	}
+	withHive(t, "./fixtures/roundtrip_query", []string{"test"}, func(h *Hive) {
+		now := timestamp()
+		oneMinuteFromNow := now.Add(time.Duration(1) * time.Minute)
+		twoMinutesFromNow := now.Add(time.Duration(2) * time.Minute)
+		err := h.Query(NewQuery([]string{"test"}, oneMinuteFromNow, twoMinutesFromNow, FilterMatchAll()))
+		if err == nil {
+			t.Error("Should not be able to query the future")
+		}
 
-	oneMinuteAgo := now.Add(time.Duration(-1) * time.Minute)
-	twoMinutesAgo := now.Add(time.Duration(-2) * time.Minute)
-	err = h.Query(NewQuery([]string{"test"}, oneMinuteAgo, twoMinutesAgo, FilterMatchAll()))
-	if err == nil {
-		t.Error("Should not be able to query start after end")
-	}
+		oneMinuteAgo := now.Add(time.Duration(-1) * time.Minute)
+		twoMinutesAgo := now.Add(time.Duration(-2) * time.Minute)
+		err = h.Query(NewQuery([]string{"test"}, oneMinuteAgo, twoMinutesAgo, FilterMatchAll()))
+		if err == nil {
+			t.Error("Should not be able to query start after end")
+		}
 
-	err = h.Query(NewQuery([]string{"nonexistentDomain"}, twoMinutesAgo, oneMinuteAgo, FilterMatchAll()))
-	if err == nil {
-		t.Error("Should not be able to query nonexistent domain")
-	}
+		err = h.Query(NewQuery([]string{"nonexistentDomain"}, twoMinutesAgo, oneMinuteAgo, FilterMatchAll()))
+		if err == nil {
+			t.Error("Should not be able to query nonexistent domain")
+		}
+	})
 }
 
 func TestQueryFilters(t *testing.T) {
-	h := hive(t, "./fixtures/roundtrip_query", []string{"test"})
-	stubLogs(t, h, []logstub{
-		logstub{"test", "foo"},
-	})
-	now := timestamp()
-	oneMinuteAgo := now.Add(time.Duration(-1) * time.Minute)
-
-	queries := []*Query{
-		NewQuery([]string{"test"}, oneMinuteAgo, now, FilterMatchAll()),
-		NewQuery([]string{"test"}, oneMinuteAgo, now, FilterExactString("foo")),
-		NewQuery([]string{"test"}, oneMinuteAgo, now, FilterContainsString("f")),
-	}
-
-	for _, q := range queries {
-		checkResults(t, h, q, []logstub{
+	withHive(t, "./fixtures/roundtrip_query", []string{"test"}, func(h *Hive) {
+		stubLogs(t, h, []logstub{
 			logstub{"test", "foo"},
 		})
-	}
+		now := timestamp()
+		oneMinuteAgo := now.Add(time.Duration(-1) * time.Minute)
 
-	negativeQueries := []*Query{
-		NewQuery([]string{"test"}, oneMinuteAgo, now, FilterExactString("hello")),
-		NewQuery([]string{"test"}, oneMinuteAgo, now, FilterContainsString("h")),
-	}
+		queries := []*Query{
+			NewQuery([]string{"test"}, oneMinuteAgo, now, FilterMatchAll()),
+			NewQuery([]string{"test"}, oneMinuteAgo, now, FilterExactString("foo")),
+			NewQuery([]string{"test"}, oneMinuteAgo, now, FilterContainsString("f")),
+		}
 
-	for _, q := range negativeQueries {
-		checkResults(t, h, q, []logstub{})
-	}
+		for _, q := range queries {
+			checkResults(t, h, q, []logstub{
+				logstub{"test", "foo"},
+			})
+		}
+
+		negativeQueries := []*Query{
+			NewQuery([]string{"test"}, oneMinuteAgo, now, FilterExactString("hello")),
+			NewQuery([]string{"test"}, oneMinuteAgo, now, FilterContainsString("h")),
+		}
+
+		for _, q := range negativeQueries {
+			checkResults(t, h, q, []logstub{})
+		}
+	})
 }
